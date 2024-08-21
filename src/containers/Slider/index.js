b70d968cb563e08a9eb4ac4084cbf53d8bb3ca28
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-08-21 16:24:33 +0200

    correction des boutons radios

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -20,37 +20,36 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+        <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
-          </div>
+           </div>
+        </div>
+      ))}    
+
           <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={event.title.id} // Modif de l'id pour éviter les erreurs de clé
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                  onChange={() => setIndex(radioIdx)} // Met à jour l'index de la carte active
-                />
-              ))}
-            </div>
+          <div className="SlideCard__pagination">
+            {byDateDesc.map((event, radioIdx) => (
+              <input
+                key={event.title}
+                type="radio"
+                name="radio-button"
+                checked={index === radioIdx}
+                onChange={() => setIndex(radioIdx)} // Met à jour l'index de la carte active
+               />
+            ))}
           </div>
-        </>
-      ))}
+        </div>
     </div>
   );
 };
