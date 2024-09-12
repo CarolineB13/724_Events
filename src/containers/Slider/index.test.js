28538ab07180aa9d79823b0325365cd931e506ae
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-09-12 11:39:18 +0200

    modification date slider 29/02 qui n'existe pas

--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -8,7 +8,7 @@ const data = {
       title: "World economic forum",
       description:
         "Oeuvre à la coopération entre le secteur public et le privé.",
-      date: "2022-02-29T20:28:45.744Z",
+      date: "2022-02-28T20:28:45.744Z",
       cover: "/images/evangeline-shaw-nwLTVwb7DbU-unsplash1.png",
     },
     {
@@ -42,7 +42,7 @@ describe("When slider is created", () => {
       </DataProvider>
     );
     await screen.findByText("World economic forum");
-    await screen.findByText("janvier");
+    await screen.findByText("février");
     await screen.findByText(
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
